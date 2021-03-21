d89c5f94fede4b181b7cb7021cec6cb330fae2d3
Johanna L <johannarleal@gmail.com>
2021-03-20 21:01:28 -0400

    Added search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,25 @@
 import React from "react";
+import "./style.css";
 
-function SearchBar(props) {
+const  SearchBar = (props) => {
   return (
     <form>
       <div className="form-group row search-bar">
         <div className="col-md-1"></div>
 
-        <div className="col-md-10">
-          {/* <label htmlFor="search">Search by employee name:</label> */}
+        <div className="col-md-4">
+          <label htmlFor="search" className="searchbar-label">Search by Employee Name:</label>
           <input
             onChange={props.handleInputChange}
             value={props.search}
             name="search"
             type="text"
             className="form-control"
-            placeholder="Search directory for an employee by entering their name"
             id="search"
           />
         </div>
 
-        <div className="col-md-1"></div>
+        <div className="col-md-7"></div>
       </div>
     </form>
   );
